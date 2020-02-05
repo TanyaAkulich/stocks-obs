ce5fd73850fc187bb47cde27c11c2f58ce3d0bc3
TanyaAkulich <akulich.tanya@gmail.com>
2020-02-05 20:15:32 +0300

    Add some stuff to log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  around_action :logging_traffic
+
+  def logging_traffic
+    logger.info "USERAGENT: #{request.headers['HTTP_USER_AGENT']}"
+    begin
+      yield
+    ensure
+      logger.info "response_status: #{response.status}"
+    end
+  end
 end
